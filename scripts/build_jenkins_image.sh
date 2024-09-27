15add57d30c8b05e5e618071c95cf68a7c366e43
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-09-27 14:44:18 +0300

    minor changes for scripts

--- scripts/build_jenkins_image.sh
+++ scripts/build_jenkins_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Removing Jenkins Docker image..."
+docker rmi jenkins-docker || true
 
 echo "Building Jenkins Docker image..."
 docker build -t jenkins-docker -f ./docker/Dockerfile_jenkins .
--- scripts/build_wt_builder.sh
+++ scripts/build_wt_builder.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "Removing WT Builder Docker image..."
 docker build -t wt_builder -f docker/Dockerfile_wt_builder .
