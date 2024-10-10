736b27b303a73ddfa3298e8b0f37a6173b48b190
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-10-10 10:58:41 +0300

    added script for launching the composed jenkins, portainer, caddy and postgres services

--- scripts/build_jenkins_image.sh
+++ scripts/build_jenkins_image.sh
@@ -4,4 +4,3 @@ docker rmi jenkins-docker || true
 
 echo "Building Jenkins Docker image..."
 docker build -t jenkins-docker -f ./docker/Dockerfile_jenkins .
-export DOCKER_SOCK_GROUP_ID=$(stat -c '%g' /var/run/docker.sock)
---
+++ scripts/compose-up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose down
+export DOCKER_SOCK_GROUP_ID=$(stat -c '%g' /var/run/docker.sock)
+docker-compose up -d
