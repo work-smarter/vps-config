54d67ea47154c1e43780d1f08f70ca1e279f1dee
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-10-10 11:56:19 +0300

    created a script to start the docker compose services and it only needs one parameter, the postgress passsword

--- scripts/compose-up.sh
+++ scripts/compose-up.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-docker compose down
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <POSTGRES_PASSWORD>"
+    exit 1
+fi
+
+POSTGRES_PASSWORD=$1
+
 export DOCKER_SOCK_GROUP_ID=$(stat -c '%g' /var/run/docker.sock)
+export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+
+docker compose down
 docker compose up -d
