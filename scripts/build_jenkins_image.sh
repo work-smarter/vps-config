418b5b387b385f212863d29c3b1902d2e8e1e95e
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-10-11 23:38:12 +0300

    change docker files folder name from docker to dockerfiles and updated the scripts

--- scripts/build_jenkins_image.sh
+++ scripts/build_jenkins_image.sh
@@ -3,4 +3,4 @@ echo "Removing Jenkins Docker image..."
 docker rmi jenkins-docker || true
 
 echo "Building Jenkins Docker image..."
-docker build -t jenkins-docker -f ./docker/Dockerfile_jenkins .
+docker build -t jenkins-docker -f ./dockerfiles/Dockerfile_jenkins .
--- scripts/build_wt_builder.sh
+++ scripts/build_wt_builder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Removing WT Builder Docker image..."
-docker build -t wt_builder -f docker/Dockerfile_wt_builder .
+docker build -t wt_builder -f dockerfiles/Dockerfile_wt_builder .
